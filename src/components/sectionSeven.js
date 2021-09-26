72795b56414ceed405ed3517bddf8de68930a818
Fabian <acostahf4@gmail.com>
2021-09-26 17:04:10 -0500

    foundation for this section is done. Need to design the form and place in the image

--- src/components/sectionSeven.js
+++ src/components/sectionSeven.js
@@ -2,16 +2,24 @@ import React from "react"
 
 const SectionSeven = () => {
   return (
-    <div>
-      <form>
-        <h1>Start Building Your Own World in Minutes </h1>
-        <p>We’re a growing team of experts and thought leaders.</p>
-        <input placeholder="First and Last Name"></input>
-        <input placeholder="Phone"></input>
-        <input placeholder="Your Company Name"></input>
-        <button>Get Started</button>
-      </form>
-      <div>image goes here</div>
+    <div className="grid grid-cols-2">
+      <div>
+        <form className="flex flex-col p-10">
+          <h1>Start Building Your Own World in Minutes </h1>
+          <p>We’re a growing team of experts and thought leaders.</p>
+          <input placeholder="First and Last Name"></input>
+          <input placeholder="Phone"></input>
+          <input placeholder="Your Company Name"></input>
+          <div className="">
+            <button className="bg-blue-800 text-white rounded-lg py-3 px-5">
+              Get Started
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="flex justify-center items-center">
+        <div>image goes here</div>
+      </div>
     </div>
   )
 }
