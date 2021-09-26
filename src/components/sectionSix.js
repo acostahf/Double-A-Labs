66700bbe8cf4f8d8f8d003ee818c3658e1261633
Fabian <acostahf4@gmail.com>
2021-09-26 16:59:58 -0500

    foundation is here just needs to be broken down into smaller components

--- src/components/sectionSix.js
+++ src/components/sectionSix.js
@@ -3,20 +3,30 @@ import React from "react"
 const SectionSix = () => {
   return (
     <div>
-      <div>
-        <h1>Meet Our Leadership Team</h1>
+      <div className="text-center pb-10">
+        <h1 className="font-black text-3xl">Meet Our Leadership Team</h1>
         <p>We’re a growing team of experts and thought leaders.</p>
       </div>
-      <div className="bg-red-900">cards goes here</div>
+      <div className="bg-red-900 p-20 text-center">cards goes here</div>
 
-      <div>
-        <div>
-          <h1>Join the Double A Labs Team</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Risus odio
-            diam egestas gravida mi. Pulvinar quisque.
-          </p>
-          <button>Join Now</button>
+      <div className="p-20 ">
+        <div className="bg-blue-500 rounded-lg p-10 flex ">
+          <div className="w-3/4">
+            <h1 className="text-white font-bold text-lg">
+              Join the Double A Labs Team
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.Risus odio
+              diam egestas gravida mi. Pulvinar quisque.
+            </p>
+          </div>
+          <div className="flex justify-center items-center w-1/4">
+            <div>
+              <button className="px-5 py-3 bg-white text-blue-700 rounded-lg ">
+                Join Now
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
