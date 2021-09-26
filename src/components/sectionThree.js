a48db3f40bf559cc2a5a161c764f5c054d40d617
Fabian <acostahf4@gmail.com>
2021-09-26 16:17:20 -0500

    set the foundation for the component. Needs tadd image and bg image

--- src/components/sectionThree.js
+++ src/components/sectionThree.js
@@ -3,25 +3,37 @@ import React from "react"
 const SectionThree = () => {
   return (
     <div>
+      <div className="grid grid-cols-2">
+        <div className="text-center">
+          <h3>Enterprise Communications</h3>
+        </div>
+        <div className="text-center">
+          <h3>Content Platforms</h3>
+        </div>
+      </div>
       <div>
-        <h3>Enterprise Communications</h3>
-        <h3>Content Platforms</h3>
-        <div>Where the image goes</div>
-        <div>
-          <div>
-            <h3>Stagnant communications</h3>
+        <div className=" flex justify-center items-center p-20">
+          <div>Where the image goes</div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2">
+        <div className="text-center pb-20">
+          <div className="pb-5">
+            <h3 className="font-bold text-blue-700">Stagnant communications</h3>
             <h3>One dimensional basic functionality attention</h3>
           </div>
-          <div>
-            <h3>“Zoom” fatigue</h3>
+          <div className="pb-5">
+            <h3 className="font-bold text-blue-700">“Zoom” fatigue</h3>
             <h3>dESIGN FLAWS THAT EXHAUST BODY and MIND</h3>
           </div>
-          <div>
-            <h3>Distracting environment</h3>
+        </div>
+        <div className="text-center pb-20">
+          <div className="pb-5">
+            <h3 className="font-bold text-blue-700">Distracting environment</h3>
             <h3>cOMPETE FOR THE VIEWER’S </h3>
           </div>
-          <div>
-            <h3>Static Interface</h3>
+          <div className="pb-5">
+            <h3 className="font-bold text-blue-700">Static Interface</h3>
             <h3>Limited Interactions</h3>
           </div>
         </div>
