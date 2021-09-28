051209bf7f9b198b92a4ec2e4ee1ac3663f83d6c
Fabian <acostahf4@gmail.com>
2021-09-28 09:35:12 -0500

    add in background images as png images

--- src/components/sectionSix.js
+++ src/components/sectionSix.js
@@ -15,23 +15,25 @@ const SectionSix = () => {
       </div>
 
       <div className="p-20 ">
-        <div className="text-white bg-gradient-to-r from-nmr-liteblue to-nmr-darkBlue rounded-xl p-10 flex ">
-          <div className="w-3/4">
-            <h1 className="font-bold text-2xl pb-3">
-              Join the Double A Labs Team
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.Risus odio
-              diam egestas gravida mi. Pulvinar quisque.
-            </p>
-          </div>
-          <div className="flex justify-center items-center w-1/4">
-            <div>
-              <button className="px-10 py-3 bg-white rounded-full ">
-                <h1 className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-nmr-liteblue to-nmr-darkBlue ">
-                  Join Now
-                </h1>
-              </button>
+        <div className="text-white bg-gradient-to-r from-nmr-liteblue to-nmr-darkBlue rounded-xl ">
+          <div className="bg-group bg-no-repeat bg-right-top p-10 flex ">
+            <div className="w-3/4">
+              <h1 className="font-bold text-2xl pb-3">
+                Join the Double A Labs Team
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.Risus
+                odio diam egestas gravida mi. Pulvinar quisque.
+              </p>
+            </div>
+            <div className="flex justify-center items-center w-1/4">
+              <div>
+                <button className="px-10 py-3 bg-white rounded-full ">
+                  <h1 className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-nmr-liteblue to-nmr-darkBlue ">
+                    Join Now
+                  </h1>
+                </button>
+              </div>
             </div>
           </div>
         </div>
