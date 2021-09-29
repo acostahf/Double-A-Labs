08e313ffb5b4b5d64bc057c38b706fe97fc673b8
Fabian <acostahf4@gmail.com>
2021-09-29 11:02:28 -0500

    adding in content on the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,29 @@
 import React from "react"
-
+import Image from "../components/Image"
 const Footer = () => {
   return (
     <div className="flex justify-center p-20 bg-footer bg-bottom  bg-no-repeat bg-cover">
-      <div className="text-white">
-        <div>icon</div>
-        <h1>DOUBLE A LABS</h1>
-        <div>menu list</div>
+      <div className="text-white flex justify-center items-center flex-col text-center">
+        <div className="pb-5">
+          <Image
+            src="whiteLogo.svg"
+            className=""
+            style={{ quality: 100, width: 30, height: 30 }}
+            alt="computer image"
+          />
+        </div>
+        <h1 className="font-bold pb-5">DOUBLE A LABS</h1>
+        <div className="flex gap-10 pb-5">
+          <div>
+            <a>Prvacy</a>
+          </div>
+          <div>
+            <a>Terms of use</a>
+          </div>
+          <div>
+            <a>Info@doublelabs.com</a>
+          </div>
+        </div>
         <div> socials</div>
       </div>
     </div>
