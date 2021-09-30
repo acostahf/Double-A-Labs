55675602747ff668c8016930fb2080723cc3c83d
Fabian <acostahf4@gmail.com>
2021-09-29 23:23:03 -0500

    navbar sticky

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Image from "../components/Image"
 
 const Header = ({ siteTitle }) => (
-  <header className="bg-white shadow-2xl">
+  <header className="bg-white shadow-2xl sticky top-0 z-10">
     <div className="container px-10 py-4 flex mx-auto justify-between items-center ">
       <div className="flex items-center gap-5">
         <Image src="logo.png" className="w-10" alt="" />
