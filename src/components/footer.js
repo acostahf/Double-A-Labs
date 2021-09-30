2e7e344a51b63efea40bf6da85a5155addd076ee
Fabian <acostahf4@gmail.com>
2021-09-29 21:24:36 -0500

    footer mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
           />
         </div>
         <h1 className="font-bold pb-5">DOUBLE A LABS</h1>
-        <div className="flex gap-10 pb-5">
+        <div className="flex flex-col lg:flex-row gap-3 lg:gap-10 pb-5">
           <div>
             <a>Prvacy</a>
           </div>
