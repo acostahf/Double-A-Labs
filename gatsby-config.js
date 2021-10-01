ab0cb1a588375479bf204abf6c3e83ffe450ca12
Fabian <acostahf4@gmail.com>
2021-10-01 11:06:04 -0500

    test image opt

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,24 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`, `avif`],
+          placeholder: `blurred`,
+          quality: 50,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`
+          tracedSVGOptions: {},
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        }
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
