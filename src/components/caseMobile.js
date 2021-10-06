6b27065fd8c92b5a70faeeaea5d9b4c8d2a3c58a
Fabian <acostahf4@gmail.com>
2021-10-06 12:33:35 -0500

    update v1

--- src/components/caseMobile.js
+++ src/components/caseMobile.js
@@ -12,7 +12,7 @@ const data = [
   {
     title: "Fan Experience",
     sub: "YouTube",
-    body: "Popular streamers and youtube influencers were some of the first to interact within Double Space. Creators squared off against one another in different contests, voted on personal favorites, and viewed new exclusive content within the Double Space.",
+    body: "Popular streamers and YouTube influencers were some of the first to interact within Double Space. Creators squared off against one another in different contests, voted on personal favorites, and viewed new exclusive content within the Double Space.",
     vid: "https://www.youtube.com/embed/XDkEDoDWDqw",
   },
   {
--- src/components/sectionFour.js
+++ src/components/sectionFour.js
@@ -9,10 +9,10 @@ const SectionFour = () => {
     <div className="flex flex-col justify-center pb-20">
       <div className="text-center px-20">
         <h1 className="text-4xl font-black pb-2">Case Studies</h1>
-        <h3 className="text-sm font-bold text-nmr-lightBlue pb-5">
+        {/* <h3 className="text-sm font-bold text-nmr-lightBlue pb-5">
           {" "}
           A PRODUCT BY DOUBE A LABS
-        </h3>
+        </h3> */}
         <div className="lg:px-36 ">
           <p className="text-gray-400 text-md">
             A deeper look into some of our most popular spaces with an emphasis
