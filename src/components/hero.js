67dbfe78f49444b3a744f3492349f7533b849176
Fabian <acostahf4@gmail.com>
2021-11-09 22:08:58 -0600

    update to header with new copy and styles.

--- src/components/hero.js
+++ src/components/hero.js
@@ -14,25 +14,33 @@ const Hero = ({ children }) => {
   return (
     <div className=" grid sm:grid-cols-2 bg-hero bg-contain bg-no-repeat  lg:py-20 py-10">
       <div className="flex flex-col justify-center p-5 sm:pl-20 lg:py-20 lg:pl-20 ">
-        <div className="xl:bg-arrow xl:bg-no-repeat xl:bg-right-bottom">
-          <h1 className="text-lg lg:text-4xl font-bold">
-            We Spark Human Connection to
+        <div className="lg:bg-arrow lg:bg-no-repeat lg:bg-right-bottom pl-5 lg:pl-0">
+          <h1 className="text-2xl lg:text-4xl font-bold">
+            This is the platform that
           </h1>
-          <h1 className="text-lg lg:text-4xl font-bold">
-            Create a Sense of Discovery that is
+          {/* <h1 className="text-lg lg:text-4xl font-bold">
+            enables Human Connection
+          </h1> */}
+          <h1 className="text-2xl lg:text-4xl font-bold">
+            enables{" "}
+            <span className="text-2xl lg:text-4xl pb-10 font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-400 via-yellow-500  to-red-400">
+              Human Connection
+            </span>
           </h1>
-          <h1 className="text-3xl lg:text-6xl pb-10 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-yellow-500  to-red-500">
-            Fun and Engaging
+          <h1 className="text-2xl lg:text-4xl font-bold">
+            and amplifies{" "}
+            <span className="text-2xl lg:text-4xl pb-10 font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-400 via-yellow-500  to-red-400">
+              Community
+            </span>
           </h1>
-          <p className=" pb-3 lg:pb-10 text-sm lg:text-xl">
-            Providing innovative virtual <br /> solutions with measurable
-            results.
+          <p className=" lg:pt-10 pb-3 lg:pb-10 text-md lg:text-xl">
+            “meet me in my metaverse”
           </p>
         </div>
-        <div className="pb-5">
+        <div className="pb-5 pl-5 lg:pl-0">
           <a href="mailto:demo@doublealabs.com">
             <button className="flex items-center gap-4 bg-gradient-to-r from-nmr-liteblue to-nmr-darkBlue rounded-lg text-white py-3 px-5 lg:text-lg">
-              Request Demo
+              Try Your Free Demo Now
               <BsArrowRight className="text-white lg:text-4xl" />
             </button>
           </a>
