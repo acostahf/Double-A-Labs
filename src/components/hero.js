361d44e1d09c02c340915876072300f8e266bab0
Fabian <acostahf4@gmail.com>
2021-11-18 00:33:29 -0600

    updates to header, resized the padding and text to get the arrow bg to position in the right spot

--- src/components/hero.js
+++ src/components/hero.js
@@ -13,31 +13,29 @@ const Hero = ({ children }) => {
 
   return (
     <div className=" grid sm:grid-cols-2 bg-hero bg-contain bg-no-repeat  lg:py-20 py-10">
-      <div className="flex flex-col justify-center p-5 sm:pl-20 lg:py-20 lg:pl-20 ">
-        <div className="lg:bg-arrow lg:bg-no-repeat lg:bg-right-bottom pl-5 lg:pl-0">
-          <h1 className="text-2xl lg:text-4xl font-bold">
+      <div className="flex flex-col justify-center p-5 sm:pl-10  lg:py-36  lg:pl-20 ">
+        <div className=" pl-5 lg:pl-0">
+          <h1 className="text-2xl lg:text-4xl xl:text-5xl font-bold">
             This is the platform that
           </h1>
-          {/* <h1 className="text-lg lg:text-4xl font-bold">
+          {/* <h1 className="text-lg lg:text- xl:text-5xl font-bold">
             enables Human Connection
           </h1> */}
-          <h1 className="text-2xl lg:text-4xl font-bold">
+          <h1 className="text-2xl lg:text-4xl xl:text-5xl font-bold">
             enables{" "}
-            <span className="text-2xl lg:text-4xl pb-10 font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-400 via-yellow-500  to-red-400">
+            <span className="text-2xl lg:text-4xl xl:text-5xl pb-10 font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-400 via-yellow-500  to-red-400">
               Human Connection
             </span>
           </h1>
-          <h1 className="text-2xl lg:text-4xl font-bold">
+          <h1 className="text-2xl lg:text-4xl xl:text-5xl font-bold">
             and amplifies{" "}
-            <span className="text-2xl lg:text-4xl pb-10 font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-400 via-yellow-500  to-red-400">
+            <span className="text-2xl lg:text-4xl xl:text-5xl pb-10 font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-400 via-yellow-500  to-red-400">
               Community
             </span>
           </h1>
-          <p className=" lg:pt-10 pb-3 lg:pb-10 text-md lg:text-xl">
-            “meet me in my metaverse”
-          </p>
         </div>
-        <div className="pb-5 pl-5 lg:pl-0">
+
+        <div className="py-5 pl-5 lg:pl-0 md:bg-arrow md:bg-no-repeat md:bg-right-bottom">
           <a href="mailto:demo@doublealabs.com">
             <button className="flex items-center gap-4 bg-gradient-to-r from-nmr-liteblue to-nmr-darkBlue rounded-lg text-white py-3 px-5 lg:text-lg">
               Try Your Free Demo Now
