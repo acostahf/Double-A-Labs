08840941008eaea432d57b0cef47178048ad9244
Fabian <acostahf4@gmail.com>
2022-02-02 13:03:39 -0600

    seo update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  purge: ["./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
